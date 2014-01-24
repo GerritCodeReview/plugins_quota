dc7ba04219d9dfb33757af2aad79a573c9a540b2
Edwin Kempin <edwin.kempin@sap.com>
2014-01-24 13:35:23 +0100

    Initial commit for quota plugin
    The quota plugin allows to enforce quotas in Gerrit.
    
    To protect a Gerrit installation it makes sense to limit the resources
    that a project or group can consume. To do this a Gerrit administrator
    can use the quota plugin to define quotas on project namespaces.
    
    With this change only a single quota for the maximum number of projects
    in a namespace is supported. This quota is only of limited use for the
    open source version of Gerrit since it doesn't allow to limit the
    project creation capability to a namespace. The idea of this quota is
    to allow a group A to create projects in the 'A/*' namespace and then
    to limit the number of projects in that namespace. E.g. if this is a
    paying customer to the number of projects that is defined in the
    contract.
    
    It is planned to add further quotas for maximum repository size and
    maximum total size of repositories in a namespace in follow-up changes.
    
    The defined quotas are stored in a `quota.config` file in the
    `refs/meta/config` branch of the `All-Project` root project.
    
    A namespace can be specified as
    * exact project name (`plugins/myPlugin`): Defines a quota for one
      project.
    * as patterns (`sandbox/*`): Defines a quota for one project
      namespace.
    * as regular expression (`^test-.*/.*`): Defines a quota for the
      namespace matching the regular expression.
    * as for-each-pattern (`public/?/*`): Defines the same quota for each
      subfolder.
    
    If a project name matches several quota namespaces the one quota
    applies to the project that is defined first in the `quota.config`
    file.
    
    Change-Id: I9b6ca2c94c3c7cdcf9b0bb0c6b91a760d8024505
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

---
+++ src/main/java/com/googlesource/gerrit/plugins/quota/MaxRepositoriesQuotaValidator.java
@@ -0,0 +1,62 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.quota;
+
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.server.project.CreateProjectArgs;
+import com.google.gerrit.server.project.ProjectCache;
+import com.google.gerrit.server.validators.ProjectCreationValidationListener;
+import com.google.gerrit.server.validators.ValidationException;
+import com.google.inject.Inject;
+
+public class MaxRepositoriesQuotaValidator implements
+    ProjectCreationValidationListener {
+  private final QuotaFinder quotaFinder;
+  private final ProjectCache projectCache;
+
+  @Inject
+  MaxRepositoriesQuotaValidator(QuotaFinder quotaFinder,
+      ProjectCache projectCache) {
+    this.quotaFinder = quotaFinder;
+    this.projectCache = projectCache;
+  }
+
+  @Override
+  public void validateNewProject(CreateProjectArgs args) throws ValidationException {
+    QuotaSection quotaSection = quotaFinder.firstMatching(args.getProject());
+    if (quotaSection != null) {
+      Integer maxProjects = quotaSection.getMaxProjects();
+      if (maxProjects != null) {
+        int count = 0;
+        for (Project.NameKey p : projectCache.all()) {
+          if (quotaSection.getNamespace().matches(p)) {
+            count++;
+          }
+        }
+        if (count >= maxProjects) {
+          StringBuilder msg = new StringBuilder();
+          msg.append("Project cannot be created because a quota for the namespace '");
+          msg.append(quotaSection.getNamespace().get());
+          msg.append("' allows at most ");
+          msg.append(maxProjects);
+          msg.append(" projects and ");
+          msg.append(count);
+          msg.append(" projects already exist.");
+          throw new ValidationException(msg.toString());
+        }
+      }
+    }
+  }
+}
---
+++ src/main/resources/Documentation/about.md
@@ -0,0 +1,68 @@
+This plugin allows to enforce quotas in Gerrit.
+
+To protect a Gerrit installation it makes sense to limit the resources
+that a project or group can consume. To do this a Gerrit administrator
+can use this plugin to define quotas on project namespaces.
+
+The following quotas are supported:
+
+* Maximum number of projects in a namespace
+
+The defined quotas are stored in a `quota.config` file in the
+`refs/meta/config` branch of the `All-Project` root project.
+Administrators can add and edit quotas by fetching this branch, editing
+the `quota.config` file locally and pushing back the changes. The
+`quota.config` file is a Git config file:
+
+```
+  [quota "sandbox/*"]
+    maxProjects = 50
+  [quota "public/*"]
+    maxProjects = 100
+```
+
+<a id="maxProjects">
+`quota.<namespace>.maxProjects`
+: The maximum number of projects that can be created in this namespace.
+
+A namespace can be specified as
+
+* exact project name (`plugins/myPlugin`): Defines a quota for one project.
+
+* as patterns (`sandbox/*`): Defines a quota for one project namespace.
+
+* as regular expression (`^test-.*/.*`): Defines a quota for the
+namespace matching the regular expression.
+
+* as for-each-pattern (`?/*`): Defines the same quota for each
+subfolder. `?` is a placeholder for any name and '?/*' with
+'maxProjects = 3' means that for every subfolder 3 projects are
+allowed. Hence '?/*' is a shortcut for having n explicit quotas:
+  '<name1>/*' with 'maxProjects = 3'
+  '<name2>/*' with 'maxProjects = 3'
+  ...
+
+
+If a project name matches several quota namespaces the one quota
+applies to the project that is defined first in the `quota.config`
+file.
+
+Example: Allow the creation of 10 projects in folder `test/*` and maximal
+500 projects in total
+
+```
+  [quota "test/*"]
+    maxProjects = 10
+  [quota "*"]
+    maxProjects = 500
+```
+
+Example: Allow the creation of 10 projects in folder `test/*` and 5
+projects in each other folder
+
+```
+  [quota "test/*"]
+    maxProjects = 10
+  [quota "?/*"]
+    maxProjects = 5
+```
