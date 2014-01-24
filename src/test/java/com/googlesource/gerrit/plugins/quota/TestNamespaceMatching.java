a1b0278811a650e1a57e02cd5a3cd8e3b693f0f0
Edwin Kempin <edwin.kempin@sap.com>
2014-01-24 13:35:23 +0100

    Initial commit for quota plugin
    The quota plugin allows to enforce quotas in Gerrit.
    
    To protect a Gerrit installation it makes sense to limit the resources
    that a project or group can consume. To do this a Gerrit administrator
    can use the quota plugin to define quotas on project namespaces.
    
    With this change only a single quota for the maximum number of projects
    in a namespace is supported. This quota is only of limited use for the
    open source version of Gerrit since it doesn't allow to limit the
    project creation capability to a namespace. The idea of this quota is
    to allow a group A to create projects in the 'A/*' namespace and then
    to limit the number of projects in that namespace. E.g. if this is a
    paying customer to the number of projects that is defined in the
    contract.
    
    It is planned to add further quotas for maximum repository size and
    maximum total size of repositories in a namespace in follow-up changes.
    
    The defined quotas are stored in a `quota.config` file in the
    `refs/meta/config` branch of the `All-Project` root project.
    
    A namespace can be specified as
    * exact project name (`plugins/myPlugin`): Defines a quota for one
      project.
    * as patterns (`sandbox/*`): Defines a quota for one project
      namespace.
    * as regular expression (`^test-.*/.*`): Defines a quota for the
      namespace matching the regular expression.
    * as for-each-pattern (`public/?/*`): Defines the same quota for each
      subfolder.
    
    If a project name matches several quota namespaces the one quota
    applies to the project that is defined first in the `quota.config`
    file.
    
    Change-Id: I9b6ca2c94c3c7cdcf9b0bb0c6b91a760d8024505
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

---
+++ src/test/java/com/googlesource/gerrit/plugins/quota/TestNamespaceMatching.java
@@ -0,0 +1,47 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.quota;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import com.google.gerrit.reviewdb.client.Project;
+
+import org.junit.Test;
+
+public class TestNamespaceMatching {
+
+  @Test
+  public void exactNamespace() {
+    String exact = "test/myProject";
+    assertTrue(new Namespace(exact).matches(new Project.NameKey("test/myProject")));
+    assertFalse(new Namespace(exact).matches(new Project.NameKey("test/myOtherProject")));
+  }
+
+  @Test
+  public void patternNamespace() {
+    String pattern = "test/*";
+    assertTrue(new Namespace(pattern).matches(new Project.NameKey("test/myProject")));
+    assertFalse(new Namespace(pattern).matches(new Project.NameKey("other/myOtherProject")));
+  }
+
+  @Test
+  public void regExp() {
+    String pattern = "^test/.*/my.*";
+    assertTrue(new Namespace(pattern).matches(new Project.NameKey("test/a/myProject")));
+    assertTrue(new Namespace(pattern).matches(new Project.NameKey("test/b/myOtherProject")));
+    assertFalse(new Namespace(pattern).matches(new Project.NameKey("other/otherProject")));
+  }
+}
