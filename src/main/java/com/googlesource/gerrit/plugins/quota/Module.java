d814e3b83b03085cce8c0956b5e3edc8fcf1e314
Stefan Lay <stefan.lay@sap.com>
2014-06-13 14:59:38 +0200

    Periodically publish fetch and push count
    Count the numbers of pushes and fetches per project and publish
    them to the registered UsageDataPublishedListeners.
    
    After publishing, the counters are reset, so that the numbers of pushes
    and fetches are counted for the time period since the last publishing
    time.
    
    Alternatives would have been to either accumulate forever or to publish
    rates. The benefit of the chosen solution is that it is then easy for a
    consumer which stores all data to accumulate sums over any interesting
    time frame.
    
    Depends-On: Ie1e82da47dc3906ef3da87dfbd0b583ebeecbef1
    Change-Id: I843c9fa7696d193d8de625d19412e50ac1a6b932

--- src/main/java/com/googlesource/gerrit/plugins/quota/Module.java
+++ src/main/java/com/googlesource/gerrit/plugins/quota/Module.java
@@ -26,6 +26,7 @@ import com.google.inject.Scopes;
 import com.google.inject.internal.UniqueAnnotations;
 
 import org.eclipse.jgit.transport.PostReceiveHook;
+import org.eclipse.jgit.transport.PreUploadHook;
 
 
 class Module extends AbstractModule {
@@ -38,7 +39,12 @@ class Module extends AbstractModule {
         .to(MaxRepositorySizeQuota.class);
     DynamicSet.bind(binder(), PostReceiveHook.class)
         .to(MaxRepositorySizeQuota.class);
+    DynamicSet.bind(binder(), PostReceiveHook.class)
+        .to(FetchAndPushCounter.class);
+    DynamicSet.bind(binder(), PreUploadHook.class)
+        .to(FetchAndPushCounter.class);
     install(MaxRepositorySizeQuota.module());
+    install(FetchAndPushCounter.module());
     install(new RestApiModule() {
       @Override
       protected void configure() {
@@ -47,6 +53,7 @@ class Module extends AbstractModule {
     });
     bind(Publisher.class).in(Scopes.SINGLETON);
     bind(PublisherScheduler.class).in(Scopes.SINGLETON);
+    bind(ProjectNameResolver.class).in(Scopes.SINGLETON);
     bind(LifecycleListener.class)
       .annotatedWith(UniqueAnnotations.create())
       .to(PublisherScheduler.class);
---
+++ src/main/java/com/googlesource/gerrit/plugins/quota/ProjectNameResolver.java
@@ -0,0 +1,54 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.quota;
+
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.server.config.GerritServerConfig;
+import com.google.gerrit.server.config.SitePaths;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import org.eclipse.jgit.lib.Config;
+import org.eclipse.jgit.lib.Repository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Path;
+
+@Singleton
+class ProjectNameResolver {
+
+  private static final Logger log = LoggerFactory
+      .getLogger(ProjectNameResolver.class);
+  private final Path basePath;
+
+  @Inject
+  ProjectNameResolver(SitePaths site, @GerritServerConfig final Config cfg) {
+    this.basePath =
+        site.resolve(cfg.getString("gerrit", null, "basePath")).toPath();
+  }
+
+  Project.NameKey projectName(Repository repo) {
+    Path gitDir = repo.getDirectory().toPath();
+    if (gitDir.startsWith(basePath)) {
+      String p = basePath.relativize(gitDir).toString();
+      String n = p.substring(0, p.length() - ".git".length());
+      return new Project.NameKey(n);
+    } else {
+      log.warn("Couldn't determine the project name from " + gitDir);
+      return null;
+    }
+  }
+}
