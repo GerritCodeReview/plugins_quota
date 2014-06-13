f4596c67eff370de23a8cd34869acb02a1a6d8b8
Stefan Lay <stefan.lay@sap.com>
2014-06-13 14:59:38 +0200

    Periodically publish fetch and push count
    Count the numbers of pushes and fetches per project and publish
    them to the registered UsageDataPublishedListeners.
    
    After publishing, the counters are reset, so that the numbers of pushes
    and fetches are counted for the time period since the last publishing
    time.
    
    Alternatives would have been to either accumulate forever or to publish
    rates. The benefit of the chosen solution is that it is then easy for a
    consumer which stores all data to accumulate sums over any interesting
    time frame.
    
    Depends-On: Ie1e82da47dc3906ef3da87dfbd0b583ebeecbef1
    Change-Id: I843c9fa7696d193d8de625d19412e50ac1a6b932

---
+++ src/main/java/com/googlesource/gerrit/plugins/quota/FetchAndPushCounter.java
@@ -0,0 +1,114 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.quota;
+
+import com.google.common.cache.Cache;
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.server.cache.CacheModule;
+import com.google.gerrit.server.config.GerritServerConfig;
+import com.google.gerrit.server.config.SitePaths;
+import com.google.inject.Inject;
+import com.google.inject.Module;
+import com.google.inject.Singleton;
+import com.google.inject.name.Named;
+
+import org.eclipse.jgit.lib.Config;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.transport.PostReceiveHook;
+import org.eclipse.jgit.transport.PreUploadHook;
+import org.eclipse.jgit.transport.ReceiveCommand;
+import org.eclipse.jgit.transport.ReceivePack;
+import org.eclipse.jgit.transport.ServiceMayNotContinueException;
+import org.eclipse.jgit.transport.UploadPack;
+
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Singleton
+public class FetchAndPushCounter implements PostReceiveHook, PreUploadHook {
+
+  static final String PUSH_COUNT_CACHE = "push_count";
+  static final String FETCH_COUNT_CACHE = "fetch_count";
+
+  static Module module() {
+    return new CacheModule() {
+      protected void configure() {
+        persist(PUSH_COUNT_CACHE, Project.NameKey.class, AtomicLong.class);
+        persist(FETCH_COUNT_CACHE, Project.NameKey.class, AtomicLong.class);
+      }
+    };
+  }
+
+  private final Cache<Project.NameKey, AtomicLong> pushCountCache;
+  private final Cache<Project.NameKey, AtomicLong> fetchCountCache;
+  private final Path basePath;
+  private final ConcurrentHashMap<String, Object> LOCKS =
+      new ConcurrentHashMap<String, Object>();
+
+  @Inject
+  FetchAndPushCounter(
+      @Named(PUSH_COUNT_CACHE) Cache<Project.NameKey, AtomicLong> pushCountCache,
+      @Named(FETCH_COUNT_CACHE) Cache<Project.NameKey, AtomicLong> fetchCountCache,
+      SitePaths site, @GerritServerConfig final Config cfg) {
+    this.pushCountCache = pushCountCache;
+    this.fetchCountCache = fetchCountCache;
+    this.basePath = site.resolve(cfg.getString("gerrit", null, "basePath")).toPath();
+  }
+
+  @Override
+  public void onBeginNegotiateRound(UploadPack up,
+      Collection<? extends ObjectId> wants, int cntOffered)
+      throws ServiceMayNotContinueException {
+    // do nothing
+  }
+
+  @Override
+  public void onEndNegotiateRound(UploadPack up,
+      Collection<? extends ObjectId> wants, int cntCommon, int cntNotFound,
+      boolean ready) throws ServiceMayNotContinueException {
+    // do nothing
+  }
+
+  @Override
+  public void onSendPack(UploadPack up, Collection<? extends ObjectId> wants,
+      Collection<? extends ObjectId> haves)
+      throws ServiceMayNotContinueException {
+    Project.NameKey project =
+        MaxRepositorySizeQuota.projectName(up.getRepository(), basePath);
+    incrementNumber(fetchCountCache, project);
+  }
+
+  @Override
+  public void onPostReceive(ReceivePack rp, Collection<ReceiveCommand> commands) {
+    Project.NameKey project =
+        MaxRepositorySizeQuota.projectName(rp.getRepository(), basePath);
+    incrementNumber(pushCountCache, project);
+  }
+
+  private void incrementNumber(Cache<Project.NameKey, AtomicLong> cache, Project.NameKey project) {
+    synchronized(LOCKS.putIfAbsent(project.get(), new Object())) {
+      AtomicLong ifPresent = cache.getIfPresent(project);
+      if (ifPresent != null) {
+        ifPresent.incrementAndGet();
+      }
+      else {
+        cache.put(project, new AtomicLong(1));
+      }
+      LOCKS.remove(project.get());
+    }
+  }
+}
