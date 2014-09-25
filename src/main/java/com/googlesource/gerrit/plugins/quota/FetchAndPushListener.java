1a7748c685ece733f9c549d65e06558b35acaab5
Adrian Görler <adrian.goerler@sap.com>
2014-09-25 09:00:19 +0200

    H2 based counter store
    The fetch and push count data is predominantely written and rarely read.
    Using a persistent cache is unsuitable for this use case.
    
    Now the counters are stored using H2 in the plugin-specific data
    directory. A cache might be added on top in a later change if the data
    is read more frequently.
    
    Change-Id: I0899e58ee7089862dcd05a8ce5fed3b7a9eaa2d5
    Signed-off-by: Adrian Görler <adrian.goerler@sap.com>

--- src/main/java/com/googlesource/gerrit/plugins/quota/FetchAndPushListener.java
+++ src/main/java/com/googlesource/gerrit/plugins/quota/FetchAndPushListener.java
@@ -4,17 +4,24 @@ import com.google.gerrit.reviewdb.client.Project;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 
+import com.googlesource.gerrit.plugins.quota.PersistentCounter.CounterException;
+
 import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.transport.PostReceiveHook;
 import org.eclipse.jgit.transport.PreUploadHook;
 import org.eclipse.jgit.transport.ReceiveCommand;
 import org.eclipse.jgit.transport.ReceivePack;
 import org.eclipse.jgit.transport.ServiceMayNotContinueException;
 import org.eclipse.jgit.transport.UploadPack;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
 
 public class FetchAndPushListener implements PostReceiveHook, PreUploadHook {
+  private static final Logger log = LoggerFactory
+      .getLogger(FetchAndPushListener.class);
 
   private final ProjectNameResolver projectNameResolver;
   private final PersistentCounter fetchCounts;
@@ -45,16 +52,21 @@ public class FetchAndPushListener implements PostReceiveHook, PreUploadHook {
   public void onSendPack(UploadPack up, Collection<? extends ObjectId> wants,
       Collection<? extends ObjectId> haves)
       throws ServiceMayNotContinueException {
-    Project.NameKey project =
-        projectNameResolver.projectName(up.getRepository());
-    fetchCounts.increment(project);
+    increment(up.getRepository(), fetchCounts);
   }
 
   @Override
   public void onPostReceive(ReceivePack rp, Collection<ReceiveCommand> commands) {
-    Project.NameKey project =
-        projectNameResolver.projectName(rp.getRepository());
-    pushCounts.increment(project);
+    increment(rp.getRepository(), pushCounts);
   }
 
+  private void increment(Repository repo, PersistentCounter counts) {
+    Project.NameKey p = projectNameResolver.projectName(repo);
+    try {
+      counts.increment(p);
+    } catch (CounterException e) {
+      log.error("can't increment counter " + counts.getKind() + " for project "
+          + p.get());
+    }
+  }
 }
