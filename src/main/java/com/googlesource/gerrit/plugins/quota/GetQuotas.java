262a8b843df5cba5fb8361914f2480c6573f71ca
Adrian Görler <adrian.goerler@sap.com>
2015-07-15 11:06:14 +0200

    Add REST endpoint to list quotas of multiple projects
    The new REST endpoint /config/server/quota~quota allows
    listing the quota information of all projects accessible
    to the caller. Using the request parameter "p", the list
    can be constrained to to return quota information for
    projects that match a given prefix only.
    
    Change-Id: I4c2d09fbc43bd2ee37f4f4ba2492d78a6737f2e8

---
+++ src/main/java/com/googlesource/gerrit/plugins/quota/GetQuotas.java
@@ -0,0 +1,58 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.quota;
+
+import com.google.gerrit.extensions.registration.DynamicMap;
+import com.google.gerrit.extensions.restapi.AuthException;
+import com.google.gerrit.extensions.restapi.ChildCollection;
+import com.google.gerrit.extensions.restapi.IdString;
+import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
+import com.google.gerrit.extensions.restapi.RestView;
+import com.google.gerrit.server.config.ConfigResource;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+
+@Singleton
+public class GetQuotas implements
+ChildCollection<ConfigResource, QuotaResource> {
+
+  private final Provider<ListQuotas> list;
+  private final DynamicMap<RestView<QuotaResource>> views;
+
+  @Inject
+  public GetQuotas(Provider<ListQuotas> list, DynamicMap<RestView<QuotaResource>> views) {
+    this.list = list;
+    this.views = views;
+  }
+
+  @Override
+  public RestView<ConfigResource> list() throws ResourceNotFoundException,
+      AuthException {
+    return list.get();
+  }
+
+  @Override
+  public QuotaResource parse(ConfigResource parent, IdString id)
+      throws ResourceNotFoundException, Exception {
+    throw new ResourceNotFoundException(id);
+  }
+
+  @Override
+  public DynamicMap<RestView<QuotaResource>> views() {
+    return views;
+  }
+
+}
