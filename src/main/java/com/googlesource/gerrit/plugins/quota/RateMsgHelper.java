0c33369318b5339fe131f32edadf777c05699c78
d048645 <viktor.kaufman@sap.com>
2017-11-06 15:22:20 +0100

    Enable messaging of permitted request bursts size
    * enable usage of the rate limiter's configuration
    * prepare for rate limit types that require the burst
      parameter and can make its use transparent to end-users
    
    Change-Id: Icfb8dafd72cc9f16166e73b74994f1174b648293

--- src/main/java/com/googlesource/gerrit/plugins/quota/RateMsgHelper.java
+++ src/main/java/com/googlesource/gerrit/plugins/quota/RateMsgHelper.java
@@ -16,26 +16,44 @@ package com.googlesource.gerrit.plugins.quota;
 
 public class RateMsgHelper {
   static final String RATE_LIMIT_TOKEN = "${rateLimit}";
+  static final String BURSTS_LIMIT_TOKEN = "${burstsLimit}";
 
   // compare AccountLimitsConfig's constructor for default rate limits
   static final String[] DEFAULT_TEMPLATE_MSG_PARTS = {
-    "Exceeded rate limit of " + RATE_LIMIT_TOKEN + " ", " requests/hour"
+    "Exceeded rate limit of " + RATE_LIMIT_TOKEN + " ",
+    " requests/hour",
+    " requests/hour (or idle time used up in bursts of max " + BURSTS_LIMIT_TOKEN + " requests)"
   };
 
   static String getDefaultTemplateMsg(String rateLimitTypeName) {
     return DEFAULT_TEMPLATE_MSG_PARTS[0] + rateLimitTypeName + DEFAULT_TEMPLATE_MSG_PARTS[1];
   }
 
+  static String getDefaultTemplateMsgWithBursts(String rateLimitTypeName) {
+    return DEFAULT_TEMPLATE_MSG_PARTS[0] + rateLimitTypeName + DEFAULT_TEMPLATE_MSG_PARTS[2];
+  }
+
   public static final String UPLOADPACK_CONFIGURABLE_MSG_ANNOTATION = "uploadpackLimitExceededMsg";
 
   private String messageFormatMsg;
+  private String messageFormatMsgWithBursts;
 
   public RateMsgHelper(String rateLimitTypeName, String templateMsg) {
     messageFormatMsg = templateMsg == null ? getDefaultTemplateMsg(rateLimitTypeName) : templateMsg;
     messageFormatMsg = messageFormatMsg.replace(RATE_LIMIT_TOKEN, "{0,number,##.##}");
+    messageFormatMsgWithBursts =
+        templateMsg == null ? getDefaultTemplateMsgWithBursts(rateLimitTypeName) : templateMsg;
+    messageFormatMsgWithBursts =
+        messageFormatMsgWithBursts
+            .replace(RATE_LIMIT_TOKEN, "{0,number,##.##}")
+            .replace(BURSTS_LIMIT_TOKEN, "{1,number,###}");
   }
 
   public String getMessageFormatMsg() {
     return messageFormatMsg;
+  }
+
+  public String getMessageFormatMsgWithBursts() {
+    return messageFormatMsgWithBursts;
   }
 }
