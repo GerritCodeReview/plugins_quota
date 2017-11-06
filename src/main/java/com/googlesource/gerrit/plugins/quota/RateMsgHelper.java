18cc09782751b6762095200047df014da33580e6
d048645 <viktor.kaufman@sap.com>
2017-11-06 11:00:17 +0100

    Refactor limit exceeded messaging
    * encapsulate the messaging functionality
    * make the configurability more transparent
    * clean up the core functionality
    * facilitate extensions, testing, etc.
    * dispose of static imports
    
    Change-Id: Ib0bd927b66e40abf15e8b3aa381e565fff765a81

---
+++ src/main/java/com/googlesource/gerrit/plugins/quota/RateMsgHelper.java
@@ -0,0 +1,50 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.quota;
+
+import com.googlesource.gerrit.plugins.quota.AccountLimitsConfig.Type;
+
+public class RateMsgHelper {
+  public static final String UPLOADPACK_CONFIGURABLE_MSG_ANNOTATION = "uploadpackLimitExceededMsg";
+  static final String RATE_LIMIT_TOKEN = "${rateLimit}";
+  static final String FORMAT_REAL = "{0,number,##.##}";
+  static final String UPLOADPACK_INLINE_NAME = "fetch";
+
+  // compare AccountLimitsConfig's constructor for default rate limits
+  static final String[] DEFAULT_TEMPLATE_MSG_PARTS = {
+    "Exceeded rate limit of " + RATE_LIMIT_TOKEN + " ", " requests/hour"
+  };
+
+  static String getDefaultTemplateMsg(String rateLimitTypeName) {
+    return DEFAULT_TEMPLATE_MSG_PARTS[0] + rateLimitTypeName + DEFAULT_TEMPLATE_MSG_PARTS[1];
+  }
+
+  private String messageFormatMsg;
+
+  public RateMsgHelper(Type limitsConfigType) {
+    this(limitsConfigType, null);
+  }
+
+  @SuppressWarnings("unused")
+  public RateMsgHelper(Type limitsConfigType, String templateMsg) {
+    messageFormatMsg =
+        templateMsg == null ? getDefaultTemplateMsg(UPLOADPACK_INLINE_NAME) : templateMsg;
+    messageFormatMsg = messageFormatMsg.replace(RATE_LIMIT_TOKEN, FORMAT_REAL);
+  }
+
+  public String getMessageFormatMsg() {
+    return messageFormatMsg;
+  }
+}
