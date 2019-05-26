0d7cfd3e6b3f30afb2d8686674ca9fd9235d1739
Paladox none <thomasmulhall410@yahoo.com>
2019-05-26 20:18:43 +0000

    Add support for blocking users in certain groups
    Change-Id: I02aa06436f315ea937e85a27d2b8ff833ef0c557

---
+++ src/main/java/com/googlesource/gerrit/plugins/quota/Blocks.java
@@ -0,0 +1,103 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.quota;
+
+import static javax.servlet.http.HttpServletResponse.SC_SERVICE_UNAVAILABLE;
+
+import com.google.common.cache.LoadingCache;
+import com.google.gerrit.httpd.AllRequestFilter;
+import com.google.gerrit.reviewdb.client.Account;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+import com.google.inject.name.Named;
+import com.googlesource.gerrit.plugins.quota.Module.Holder;
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+public class Blocks extends AllRequestFilter {
+  private static final Logger log = LoggerFactory.getLogger(Blocks.class);
+
+  private static final String GIT_UPLOAD_PACK_PROTOCOL = "/git-upload-pack";
+  private static final String LOGIN_PREFIX = "/login";
+  private static final String LOGIN_INFIX = LOGIN_PREFIX + "/";
+
+  private final Provider<CurrentUser> user;
+  private final IdentifiedUser.GenericFactory userFactory;
+  private final AccountLimitsFinder finder;
+
+  @Inject
+  Blocks(
+      Provider<CurrentUser> user,
+      IdentifiedUser.GenericFactory userFactory,
+      AccountLimitsFinder finder) {
+    this.user = user;
+    this.userFactory = userFactory;
+    this.finder = finder;
+  }
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+      doFilter((HttpServletRequest) request, (HttpServletResponse) response, chain);
+  }
+
+  private void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+    System.out.println(getBlockValue());
+      System.out.println(shouldBlock(request));
+      if (getBlockValue() && shouldBlock(request)) {
+          response.sendError(HttpServletResponse.SC_FORBIDDEN, "You have been blocked!");
+          return;
+      }
+      chain.doFilter(request, response);
+  }
+
+  private boolean shouldBlock(HttpServletRequest request) {
+    String method = request.getMethod();
+    String servletPath = request.getServletPath();
+    return (("POST".equals(method)
+                && !servletPath.endsWith(GIT_UPLOAD_PACK_PROTOCOL)
+                && !servletPath.equals(LOGIN_PREFIX)
+                && !servletPath.contains(LOGIN_INFIX))
+            || "PUT".equals(method)
+            || "DELETE".equals(method));
+  }
+
+  private boolean getBlockValue() {
+    CurrentUser u = user.get();
+    if (u.isIdentifiedUser()) {
+      Account.Id accountId = u.asIdentifiedUser().getAccountId();
+      Optional<AccountLimitsConfig.Block> blocks = finder.firstMatchingBlock(AccountLimitsConfig.Type.BLOCK, userFactory.create(accountId));
+      if (blocks.isPresent()) {
+        return blocks.get().getBlockValue();
+      }
+    }
+    return false;
+  }
+}
