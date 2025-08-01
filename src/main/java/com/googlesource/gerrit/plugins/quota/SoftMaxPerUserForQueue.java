f797f154b0705ca14f96db898f1f811a1c622b2c
Adithya Chakilam <achakilam@nvidia.com>
2025-07-31 23:13:37 -0500

    Improve thread pool sharing with soft max
    Previously, interactive users—especially during bulk operations like
    repository syncs—could saturate the entire interactive thread pool,
    blocking other users from running their tasks.
    
    While the `maxStartPerUserForTaskForQueue` config could restrict how
    many tasks a single user starts, it also limits full pool utilization
    when no other users are active.
    
    This change introduces the concept of a soft max, allowing more flexible
    and fair sharing of the thread pool:
    - A user can exceed softMax if the pool is otherwise idle.
    - When new users arrive and start tasks, the original user's share is
      gradually reduced until soft max to make room for others.
    
    This is achieved by ensuring at least two threads are available to start
    a task (if the user is beyond the `softMax`), but only one is consumed,
    allowing the scheduler to prioritize new users fairly.
    
    This dynamic parking mechanism prevents background-heavy users from
    monopolizing the pool, ensuring interactive users aren't blocked and
    improving overall responsiveness.
    
    Change-Id: I327416604aeeb36856b1990f0cf89e0a77757e82

---
+++ src/main/java/com/googlesource/gerrit/plugins/quota/SoftMaxPerUserForQueue.java
@@ -0,0 +1,90 @@
+package com.googlesource.gerrit.plugins.quota;
+
+import com.google.gerrit.server.git.WorkQueue;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.googlesource.gerrit.plugins.quota.TaskParser.user;
+
+public class SoftMaxPerUserForQueue extends TaskQuota {
+  public static final Map<String, Function<BuildInfo, Integer>> QUEUES =
+      Map.of(
+          "SSH-Interactive-Worker",
+          BuildInfo::interactiveThreads,
+          "SSH-Batch-Worker",
+          BuildInfo::batchThreads);
+  public static final Pattern CONFIG_PATTERN =
+      Pattern.compile("(\\d+)\\s+(" + String.join("|", QUEUES.keySet()) + ")");
+  private final int softMax;
+  private final String queueName;
+  private final ConcurrentHashMap<String, Integer> taskStartedCountByUser =
+      new ConcurrentHashMap<>();
+
+  public SoftMaxPerUserForQueue(int maxPermits, int softMax, String queueName) {
+    super(maxPermits);
+    this.softMax = softMax;
+    this.queueName = queueName;
+  }
+
+  @Override
+  public boolean isApplicable(WorkQueue.Task<?> task) {
+    return task.getQueueName().equals(queueName);
+  }
+
+  @Override
+  public boolean tryAcquire(WorkQueue.Task<?> task) {
+    return user(task)
+        .map(
+            user -> {
+              AtomicBoolean acquired = new AtomicBoolean(false);
+              taskStartedCountByUser.compute(
+                  user,
+                  (key, val) -> {
+                    int runningTasks = (val != null) ? val : 0;
+                    boolean overSoftLimit = runningTasks >= softMax;
+                    int permitCost = overSoftLimit ? 2 : 1;
+
+                    if (permits.tryAcquire(permitCost)) {
+                      acquired.setPlain(true);
+                      if (overSoftLimit) {
+                        permits.release(1);
+                      }
+                      ++runningTasks;
+                    }
+                    return runningTasks;
+                  });
+              return acquired.getPlain();
+            })
+        .orElse(true);
+  }
+
+  @Override
+  public void release(WorkQueue.Task<?> task) {
+    user(task)
+        .ifPresent(
+            user ->
+                taskStartedCountByUser.computeIfPresent(
+                    user,
+                    (u, tasks) -> {
+                      permits.release(1);
+                      return tasks == 1 ? null : --tasks;
+                    }));
+  }
+
+  public static Optional<TaskQuota> build(BuildInfo buildInfo) {
+    Matcher matcher = CONFIG_PATTERN.matcher(buildInfo.config());
+    return matcher.find()
+        ? Optional.of(
+            new SoftMaxPerUserForQueue(
+                QUEUES.get(matcher.group(2)).apply(buildInfo),
+                Integer.parseInt(matcher.group(1)),
+                matcher.group(2)))
+        : Optional.empty();
+  }
+}
--- src/main/java/com/googlesource/gerrit/plugins/quota/TaskQuotaKeys.java
+++ src/main/java/com/googlesource/gerrit/plugins/quota/TaskQuotaKeys.java
@@ -8,12 +8,13 @@ public enum TaskQuotaKeys {
   MAX_START_FOR_TASK_FOR_USER_FOR_QUEUE(
       "maxStartForTaskForUserForQueue", TaskQuotaForTaskForQueueForUser::build),
   MAX_START_PER_USER_FOR_TASK_FOR_QUEUE(
-      "maxStartPerUserForTaskForQueue", TaskQuotaPerUserForTaskForQueue::build);
+      "maxStartPerUserForTaskForQueue", TaskQuotaPerUserForTaskForQueue::build),
+  SOFT_MAX_START_FOR_QUEUE_PER_USER("softMaxStartPerUserForQueue", SoftMaxPerUserForQueue::build);
 
   public final String key;
-  public final Function<String, Optional<TaskQuota>> processor;
+  public final Function<TaskQuota.BuildInfo, Optional<TaskQuota>> processor;
 
-  TaskQuotaKeys(String key, Function<String, Optional<TaskQuota>> processor) {
+  TaskQuotaKeys(String key, Function<TaskQuota.BuildInfo, Optional<TaskQuota>> processor) {
     this.key = key;
     this.processor = processor;
   }
