801d734fd6e3e53a302409c84fc4d45ad61c0c0e
Adithya Chakilam <achakilam@nvidia.com>
2025-08-23 22:07:52 -0500

    Only have a single shared free threads/permits for soft max
    ```
    [quota "project-a"]
        softMaxStartPerUserForQueue = 5 SSH-Interactive-Worker
    [quota "project-b"]
        softMaxStartPerUserForQueue = 10 SSH-Interactive-Worker
    ```
    
    Previously, for the above configuration, two separate TaskQuota (free
    threads) instances were created in the background. The computation of
    free threads was buggy since its not shared. Each instance is not aware
    of the permits acquired by the other quota. This results in config not
    being enforced correctly. Fix this to only have single soft max quota
    per queue which has details of all the tasks that are being ran in the
    queue.
    
    Change-Id: Ib8e82fc413683647b9ef730819121bcb2d42b5c2

--- src/main/java/com/googlesource/gerrit/plugins/quota/TaskQuotaKeys.java
+++ src/main/java/com/googlesource/gerrit/plugins/quota/TaskQuotaKeys.java
@@ -9,7 +9,7 @@ public enum TaskQuotaKeys {
       "maxStartForTaskForUserForQueue", TaskQuotaForTaskForQueueForUser::build),
   MAX_START_PER_USER_FOR_TASK_FOR_QUEUE(
       "maxStartPerUserForTaskForQueue", TaskQuotaPerUserForTaskForQueue::build),
-  SOFT_MAX_START_FOR_QUEUE_PER_USER("softMaxStartPerUserForQueue", SoftMaxPerUserForQueue::build);
+  SOFT_MAX_START_FOR_QUEUE_PER_USER("softMaxStartPerUserForQueue", SoftMaxBuilder::record);
 
   public final String key;
   public final Function<TaskQuota.BuildInfo, Optional<TaskQuota>> processor;
