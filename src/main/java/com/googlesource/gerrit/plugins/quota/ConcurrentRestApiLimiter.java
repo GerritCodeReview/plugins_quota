654970c45f4f559ba9fa2b9d2abc2475f378a098
Adithya Chakilam <achakilam@nvidia.com>
2025-09-24 13:44:21 -0500

    Ability to limit conccurent rest api calls
    Eventhough we have ratelimitng over a window of period, costly restapis
    ran concurrently by users could still bring down the server. Have a way
    to limit concurrent requests per user. Only applies to restapis.
    
    Change-Id: I0a89cf31199ae9c610be8b32e47d7b122826b605

---
+++ src/main/java/com/googlesource/gerrit/plugins/quota/ConcurrentRestApiLimiter.java
@@ -0,0 +1,107 @@
+package com.googlesource.gerrit.plugins.quota;
+
+import com.google.gerrit.httpd.AllRequestFilter;
+import com.google.gerrit.server.CurrentUser;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Pattern;
+
+@Singleton
+public class ConcurrentRestApiLimiter extends AllRequestFilter {
+  private static final Pattern REST_API_PATH_PATTERN =
+      Pattern.compile(
+          "^/(?:a/)?(access|accounts|changes|config|groups|plugins|projects|tools)/.*$");
+
+  private final Map<String, Integer> connectionsByUser = new ConcurrentHashMap<>();
+  private final Provider<CurrentUser> userProvider;
+  private final AccountLimitsFinder accountLimitsFinder;
+
+  @Inject
+  public ConcurrentRestApiLimiter(
+      AccountLimitsFinder accountLimitsFinder, Provider<CurrentUser> userProvider) {
+    this.accountLimitsFinder = accountLimitsFinder;
+    this.userProvider = userProvider;
+  }
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+    if (isRestApiRequest(request)) {
+      CurrentUser currentUser = userProvider.get();
+
+      if (currentUser.isIdentifiedUser()) {
+        String userId = currentUser.getAccountId().toString();
+        Optional<Integer> limit = getEffectiveLimit(currentUser);
+
+        if (limit.isPresent()) {
+          try {
+            if (!canPermitCall(userId, limit.get())) {
+              ((HttpServletResponse) response)
+                  .sendError(
+                      429, "Too Many Requests: rate limited by maxConcurrentRestApiCallsPerUser");
+              return;
+            }
+            chain.doFilter(request, response);
+          } finally {
+            markCallComplete(userId);
+          }
+          return;
+        }
+      }
+    }
+
+    chain.doFilter(request, response);
+  }
+
+  private boolean isRestApiRequest(ServletRequest req) {
+    return req instanceof HttpServletRequest
+        && REST_API_PATH_PATTERN.matcher(((HttpServletRequest) req).getServletPath()).matches();
+  }
+
+  private Optional<Integer> getEffectiveLimit(CurrentUser user) {
+    List<Integer> limits = new ArrayList<>();
+
+    accountLimitsFinder
+        .firstMatching(AccountLimitsConfig.Type.CONCURRENT_RESTAPI, user.asIdentifiedUser())
+        .ifPresent(rl -> limits.add(rl.getConcurrentRestApiRequests()));
+
+    accountLimitsFinder
+        .getGlobalRateLimit(AccountLimitsConfig.Type.CONCURRENT_RESTAPI)
+        .ifPresent(rl -> limits.add(rl.getConcurrentRestApiRequests()));
+
+    return limits.stream().filter(l -> l > 0).min(Integer::compareTo);
+  }
+
+  private boolean canPermitCall(String userId, int limit) {
+    return connectionsByUser.compute(
+            userId,
+            (k, v) -> {
+              if (v == null || v < limit) {
+                return (v == null) ? 1 : v + 1;
+              } else {
+                return v;
+              }
+            })
+        <= limit;
+  }
+
+  private void markCallComplete(String userId) {
+    connectionsByUser.computeIfPresent(
+        userId,
+        (k, v) -> {
+          int updated = v - 1;
+          return (updated <= 0) ? null : updated;
+        });
+  }
+}
