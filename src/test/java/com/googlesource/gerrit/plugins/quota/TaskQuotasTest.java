e9773c13dbbce7bae13017deda07ea1de8f0c278
Adithya Chakilam <achakilam@nvidia.com>
2025-10-24 16:15:12 -0500

    Introduce test cases for quotas
    Adds basic framework and test case for `maxStartForTaskForQueue`.
    
    Change-Id: I96b37d18e87664e968253f5569f0a715535d7f22

---
+++ src/test/java/com/googlesource/gerrit/plugins/quota/TaskQuotasTest.java
@@ -0,0 +1,108 @@
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.quota;
+
+import static com.googlesource.gerrit.plugins.quota.QueueStats.Queue.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+
+import com.google.gerrit.server.git.WorkQueue;
+import java.util.Random;
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.Config;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TaskQuotasTest {
+  private static final String PROJECT_X = "project-x";
+  private static final String USER_A = "USER_A";
+
+  @Test
+  public void testMaxStartForTaskForQueue() throws ConfigInvalidException {
+    TaskQuotas taskQuotas =
+        taskQuotas(
+            2,
+            2,
+"""
+[quota "%s"]
+  maxStartForTaskForQueue = 1 uploadpack %s
+"""
+                .formatted(PROJECT_X, INTERACTIVE.getName()));
+
+    WorkQueue.Task<?> upX1 = task(INTERACTIVE.getName(), uploadPackTask(PROJECT_X, USER_A));
+    WorkQueue.Task<?> upX2 = task(INTERACTIVE.getName(), uploadPackTask(PROJECT_X, USER_A));
+    WorkQueue.Task<?> upX3 = task(INTERACTIVE.getName(), uploadPackTask(PROJECT_X, USER_A));
+
+    WorkQueue.Task<?> rpX1 = task(INTERACTIVE.getName(), receivePackTask(PROJECT_X, USER_A));
+    WorkQueue.Task<?> rpX2 = task(INTERACTIVE.getName(), receivePackTask(PROJECT_X, USER_A));
+    WorkQueue.Task<?> rpX3 = task(INTERACTIVE.getName(), receivePackTask(PROJECT_X, USER_A));
+
+    assertTrue(taskQuotas.isReadyToStart(upX1));
+    startAndCompleteTask(taskQuotas, upX1);
+
+    assertTrue(taskQuotas.isReadyToStart(rpX1));
+    startAndCompleteTask(taskQuotas, rpX1);
+
+    assertTrue(taskQuotas.isReadyToStart(upX2));
+
+    assertTrue(taskQuotas.isReadyToStart(rpX2));
+    startAndCompleteTask(taskQuotas, rpX2);
+
+    assertFalse(taskQuotas.isReadyToStart(upX3));
+    taskQuotas.onNotReadyToStart(upX3);
+
+    assertTrue(taskQuotas.isReadyToStart(rpX3));
+    startAndCompleteTask(taskQuotas, rpX3);
+
+    startAndCompleteTask(taskQuotas, upX2);
+
+    assertTrue(taskQuotas.isReadyToStart(upX3));
+    startAndCompleteTask(taskQuotas, upX3);
+  }
+
+  private WorkQueue.Task<?> task(String queueName, String taskString) {
+    WorkQueue.Task<?> task = Mockito.mock(WorkQueue.Task.class);
+    when(task.getTaskId()).thenReturn(new Random().nextInt());
+    when(task.getQueueName()).thenReturn(queueName);
+    when(task.toString()).thenReturn(taskString);
+    return task;
+  }
+
+  private TaskQuotas taskQuotas(int interactiveThreads, int batchThreads, String cfg)
+      throws ConfigInvalidException {
+    Config quotaConfig = new Config();
+    quotaConfig.fromText(cfg);
+    QuotaFinder finder = spy(new QuotaFinder(null));
+    doReturn(quotaConfig).when(finder).getQuotaConfig();
+    return new TaskQuotas(finder, interactiveThreads, batchThreads);
+  }
+
+  private String uploadPackTask(String project, String user) {
+    return "git-upload-pack %s (%s)".formatted(project, user);
+  }
+
+  private String receivePackTask(String project, String user) {
+    return "git-receive-pack %s (%s)".formatted(project, user);
+  }
+
+  private void startAndCompleteTask(TaskQuotas quotas, WorkQueue.Task<?> task) {
+    quotas.onStart(task);
+    quotas.onStop(task);
+  }
+}
